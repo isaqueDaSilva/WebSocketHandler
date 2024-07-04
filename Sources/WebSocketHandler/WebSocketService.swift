8f33422c6de6db04917ae0e83ee4e10e5445edc6
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-07-04 05:36:12 -0300

    Some updates

--- Sources/WebSocketHandler/WebSocketService.swift
+++ Sources/WebSocketHandler/WebSocketService.swift
@@ -33,9 +33,10 @@ public struct WebSocketService<ReceiveMessage: Decodable> {
     /// A default WebSocket Upgrader for this aplication.
     private var wsUpgrader: EventLoopFuture<UpgradeResult>?
     
+    private var outboundWriter: NIOAsyncChannelOutboundWriter<WebSocketFrame>?
+    
     /// The default Combine's subject that stores the current message or an error to send back to the top level aplication.
     public let messageReceivedSubject: PassthroughSubject<ReceiveMessage, WebSocketError>
-    
     
     /// Starts the WebSocket channel
     public mutating func start<M: Codable>(with initialMessage: M) async throws {
@@ -136,9 +137,12 @@ public struct WebSocketService<ReceiveMessage: Decodable> {
         
         // Executes the channel.
         try await channel.executeThenClose { inbound, outbound in
-            // Sends the initial message for the channel.
+            outboundWriter = outbound
             
-            try await outbound.write(message)
+            // Sends the initial message for the channel.
+            guard let outboundWriter else { return }
+            
+            try await outboundWriter.write(message)
             
             // Creates a stream for receive
             // the all data that coming
@@ -147,7 +151,10 @@ public struct WebSocketService<ReceiveMessage: Decodable> {
                 switch frame.opcode {
                 case .binary:
                     onReceive(frame.data)
+                case .ping:
+                    print("Pong Received: \(String(buffer: frame.data))")
                 default:
+                    print("Message received isn't valid.")
                     break
                 }
             }
@@ -185,62 +192,44 @@ public struct WebSocketService<ReceiveMessage: Decodable> {
     }
     
     /// Sends a ping frame for the WebSocket channel.
-    public func sendPing() async throws {
+    public mutating func sendPing() async throws {
         let pingFrame = WebSocketFrame(fin: true, opcode: .ping, data: .init(string: "I'm here."))
         
-        guard let wsUpgrader else { return }
+        guard wsUpgrader != nil else { return }
         
-        let channel = try await wsUpgrader.get()
-        
-        switch channel {
-        case .websocket(let wsChannel):
-            try await wsChannel.executeThenClose { inbound, outbound in
-                try await outbound.write(pingFrame)
-                
-                for try await pong in inbound {
-                    switch pong.opcode {
-                    case .pong:
-                        print("Pong Received: \(String(buffer: pong.data))")
-                    case .binary:
-                        onReceive(pong.data)
-                    default:
-                        messageReceivedSubject.send(completion: .failure(.dataNotSuported))
-                        return
-                    }
-                }
-            }
-        case .notUpgraded:
+        guard let outboundWriter else {
             messageReceivedSubject.send(completion: .failure(.noConnection))
+            try await disconnect()
+            return
+        }
+        
+        do {
+            try await outboundWriter.write(pingFrame)
+        } catch {
+            messageReceivedSubject.send(completion: .failure(.unknownError(error)))
+            try await disconnect()
         }
     }
     
     /// Sends a message for the WebSocket channel
     /// - Parameter message: The message data representation for send to the channel.
-    public func send(_ message: Data) async throws {
+    public mutating func send(_ message: Data) async throws {
         let messageByte = [UInt8](message)
         let messageFrame = WebSocketFrame(fin: true, opcode: .binary, data: .init(bytes: messageByte))
         
-        guard let wsUpgrader else { throw WebSocketError.noConnection }
+        guard wsUpgrader != nil else { return }
         
-        let channel = try await wsUpgrader.get()
-        
-        switch channel {
-        case .websocket(let wsChannel):
-            try await wsChannel.executeThenClose { inbound, outbound in
-                try await outbound.write(messageFrame)
-                
-                for try await message in inbound {
-                    switch message.opcode {
-                    case .binary:
-                        onReceive(message.data)
-                    default:
-                        messageReceivedSubject.send(completion: .failure(.dataNotSuported))
-                        return
-                    }
-                }
-            }
-        case .notUpgraded:
+        guard let outboundWriter else {
             messageReceivedSubject.send(completion: .failure(.noConnection))
+            try await disconnect()
+            return
+        }
+        
+        do {
+            try await outboundWriter.write(messageFrame)
+        } catch {
+            messageReceivedSubject.send(completion: .failure(.unknownError(error)))
+            try await disconnect()
         }
     }
     
